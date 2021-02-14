e024c5d2bc90abd61509aad5bcb694434b64bddb
Souliousery <24778461+SoulRepo@users.noreply.github.com>
2021-02-13 19:27:58 -0500

    added direct link to trade faster

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -22,7 +22,7 @@ export const CTA = () => (
         Discord
       </Button>
     </Link>
-    <Link isExternal href="https://app.honeyswap.org/#/swap" flexGrow={1} mx={2}>
+    <Link isExternal href="https://app.honeyswap.org/#/swap?outputCurrency=0xeaf7b3376173df8bc0c22ad6126943cc8353c1ee" flexGrow={1} mx={2}>
       <Button width="100%" colorScheme="orange" variant="outline">
         Trade on Honeyswap
       </Button>
