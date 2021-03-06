2a8764bb4892bd97086ae0e060c980f1230aa85b
kowasaur <kowasaur@gmail.com>
2021-03-06 17:56:48 +1000

    basically finish fah

--- fah/fah.py
+++ fah/fah.py
@@ -1,60 +1,48 @@
 import requests
+from csv import DictWriter
 from cryptoaddress import EthereumAddress
 
-# Cheemscoin one is 1060762
-TEAMID = "226715" # this is the dogefolders one just for testing
+TEAMID = "1060762"
 # ! remember to change this
 TOTALCHEEMS = 5000
 # TODO: Consider a minimum amount of CHEEMS per user
 # TODO: Also consider giving a bonus to people that have supplied liquidity
 
 with open('fah/previous.txt', 'r') as file:
-  oldScores = list(file.read())
+  oldScores = eval(file.read())
 
 response = requests.get("https://stats.foldingathome.org/api/team/" + TEAMID)
 scores = response.json()["donors"]
 
-# Fake data for testing
-scores.append({
-  "name": "0xB67D4dd9F0E25760dC0f373d79588Bd0169b2335",
-  "credit": 69
-})
-scores.append({
-  "name": "0xEaF7B3376173DF8BC0C22Ad6126943cC8353C1Ee",
-  "credit": 690
-})
-scores.append({
-  "name": "0x9f27D1Dc4702711D16E9CdeFE5CCA633Edc46154",
-  "credit": 420
-})
-
-validUsers = []
+validScores = {}
 invalidUsers = []
-totalPoints = 0
 
 for user in scores:
   try:
     EthereumAddress(user["name"])
-    validUsers.append({
-      "address": user["name"],
-      "score": user["credit"]
-    })
-    totalPoints += user["credit"]
+    validScores[user["name"]] = user["credit"]
   except ValueError:
     invalidUsers.append(user)
 
-with open('fah/previous.txt', 'w') as file:
-  file.write(str(validUsers))
+if validScores == oldScores:
+  raise Exception("Valid scores identical to previous")
 
-# TODO: Figure out difference from last run and this run
-# Basically so the score is only for this week
+with open('fah/previous.txt', 'w') as file:
+  file.write(str(validScores))
+
+weekScores = {k: v - oldScores.get(k, 0) for (k, v) in validScores.items() if v > oldScores.get(k, 0)}
+totalPoints = sum(weekScores.values())
 
 cheemsAmounts = list(map((lambda user: {
-  "address": user["address"],
-  "cheems": user["score"] / totalPoints * TOTALCHEEMS
-}), validUsers))
+  "address": user[0],
+  "points": user[1],
+  "cheems": user[1] / totalPoints * TOTALCHEEMS
+}), weekScores.items()))
 
-print(cheemsAmounts)
+with open("fah/payout.csv", 'w', encoding="utf8", newline="") as output:
+  dict_writer = DictWriter(output, cheemsAmounts[0].keys())
+  dict_writer.writeheader()
+  dict_writer.writerows(cheemsAmounts)
 
-# print('Invalid users:')
-# print(invalidUsers)
+print('Invalid users:')
+print(invalidUsers)
