386c985525858e67c68937be2f3ec168436bb030
kowasaur <kowasaur@gmail.com>
2021-04-04 17:06:47 +1000

    Comment TokenLock.sol

--- contracts/TokenLock.sol
+++ contracts/TokenLock.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+// https://cheemsco.in
 pragma solidity 0.7.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
-
-// note: all time is in seconds
 
 struct lock {
   IERC20 token;
@@ -16,21 +15,37 @@ struct lock {
 
 uint256 constant BUFFER = 3 days;
 
+/// @title ERC20 token locker
+/// @author kowasaur
+/// @notice Owner can lock tokens for specified period of time (lock meaning they can't withdraw)
+/// @notice There's a 3 day buffer between requesting to unlock and being able to
+/// @dev all time is in seconds
 contract TokenLock is Ownable {
   using SafeMath for uint256;
 
   lock[] public locks;
 
+  /// @notice Locks a token for the specified duration
+  /// @param token an ERC20 token
+  /// @param amount amount of token to be locked in wei
+  /// @param duration how long the lock will last for
   function createLock(
     IERC20 token,
     uint256 amount,
     uint256 duration
-  ) public onlyOwner {
+  ) external onlyOwner {
     token.transferFrom(msg.sender, address(this), amount);
     locks.push(lock(token, amount, block.timestamp.add(duration), 0));
   }
 
-  function unlock(uint256 lockIndex, address recipient) public onlyOwner {
+  /** 
+    @notice Sends the locked tokens to the recipient if the 3 day buffer has passed.
+    If the owner hasn't requested to unlock, it creates the 3 day buffer instead. 
+    If the lock duration hasn't finished, it fails. 
+  */
+  /// @param lockIndex index of the lock in locks
+  /// @param recipient the address of who receives the tokens
+  function unlock(uint256 lockIndex, address recipient) external onlyOwner {
     lock memory l = locks[lockIndex];
     require(block.timestamp >= l.requestTime, "Can not unlock early");
 
@@ -43,7 +58,10 @@ contract TokenLock is Ownable {
     }
   }
 
-  function extendLock(uint256 lockIndex, uint256 durationExtension) public onlyOwner {
+  /// @notice Adds time to a lock
+  /// @param lockIndex index of the lock in locks
+  /// @param durationExtension time to be added to the lock
+  function extendLock(uint256 lockIndex, uint256 durationExtension) external onlyOwner {
     lock memory l = locks[lockIndex];
     l.requestTime.add(durationExtension);
     locks[lockIndex] = l;
