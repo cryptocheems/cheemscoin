ea373b064ca890d1b4ade41e109a34c4fa7e3153
kowasaur <kowasaur@gmail.com>
2021-04-06 17:47:55 +1000

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// * This assumes your name in package.json is the same as your repo name
-// * and your author name is the same as your github username
-
-const isProd = process.env.NODE_ENV === "production";
-const { name, author } = require("./package.json");
-
-module.exports = {
-  assetPrefix: isProd ? `https://cdn.statically.io/gh/${author.name}/${name}/gh-pages/` : "",
-};
