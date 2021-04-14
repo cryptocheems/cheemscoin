b3e774245b86d24c9f9ebfbf42213b02aa9c5f49
kowasaur <kowasaur@gmail.com>
2021-04-14 12:43:42 +1000

    improve farm contract
    and create migration

--- contracts/LockedCheemscoinLpFarm.sol
+++ contracts/LockedCheemscoinLpFarm.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-
+// https://cheemsco.in
 pragma solidity ^0.7.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -13,12 +13,20 @@ struct stake {
   // In cheemscoin
   uint256 reward;
   uint256 claimTime;
+  // If it's hard locked it can't be withdrawn until the specified time
+  // But if it's soft locked it can be withdrawn early but the reward is forfeited
   bool isHard;
 }
 
 uint256 constant BASEREWARD = 5 * 10**10;
 uint256 constant MINDURATION = 7 days;
 
+/// @title Transferable Cheemscoin LP Token Staker
+/// @author kowasaur
+/**
+ * @notice Lock up Cheemscoin LP Tokens and earn Cheemscoin
+ * Each "stake" is represented as an ERC721 so you can transfer them
+ */
 /// @dev all time is in seconds
 contract LockedCheemscoinLpFarm is ERC721 {
   using SafeMath for uint256;
@@ -29,7 +37,9 @@ contract LockedCheemscoinLpFarm is ERC721 {
   IERC20 private _lp;
   IERC20 private _cheemscoin;
 
+  /// @notice The stake information for each token
   mapping(uint256 => stake) public stakes;
+  /// @notice Amount of Cheemscoin that hasn't been allocated yet
   uint256 public availableCheems = 0;
 
   constructor(IERC20 cheemscoin, IERC20 lpToken) ERC721("Locked Cheemscoin LP", "lchLP") {
@@ -41,6 +51,7 @@ contract LockedCheemscoinLpFarm is ERC721 {
     return block.timestamp - _startTime;
   }
 
+  /// @notice Calculates how much Cheemscoin should be rewarded
   /// @param amount amount of tokens in wei
   /// @param duration how long the lock will last for
   /// @param isHard whether the stake is hard or soft locked
@@ -49,15 +60,17 @@ contract LockedCheemscoinLpFarm is ERC721 {
     uint256 duration,
     bool isHard
   ) public view returns (uint256) {
-    uint256 hardBonus = isHard ? 15 weeks : 60 weeks;
+    // Don't worry, this maths confuses me too
+    uint256 hardBonus = isHard ? 26 weeks : 52 weeks;
     uint256 r = BASEREWARD.sub(timeFromStart().mul(10**3).div(2));
-    uint256 a = amount.mul(r).div(10**11);
+    uint256 a = amount.mul(r).div(10**12).mul(isHard ? 11 : 10);
     uint256 bonus = a.mul(duration**2).div(hardBonus.add(totalSupply().mul(1.5 days)));
     return a.mul(duration).add(bonus).div(1 weeks);
   }
 
-  /// @param amount amount of lp token to be locked in wei
-  /// @param duration how long the lock will last for
+  /// @notice Stakes lp tokens
+  /// @param amount amount of lp token to be staked in wei
+  /// @param duration how long the stake will last for
   /// @param isHard whether the stake is hard or soft locked
   function lock(
     uint256 amount,
@@ -77,20 +90,20 @@ contract LockedCheemscoinLpFarm is ERC721 {
     stakes[newId] = stake(amount, reward, block.timestamp.add(duration), isHard);
   }
 
+  /// @notice Redeems staked lp tokens
   /// @param id Token to be redeemed
   function redeem(uint256 id) external {
     require(_isApprovedOrOwner(msg.sender, id), "Missing permission to manage token");
     stake memory token = stakes[id];
     bool notEarly = token.claimTime <= block.timestamp;
-    if (token.isHard) {
-      require(notEarly, "Can not redeem early");
-    }
-    // TODO Perhaps slash a bit of liquidity if early withdrawal
-    if (notEarly) {
-      _cheemscoin.transfer(msg.sender, token.reward);
-    }
+    if (token.isHard) require(notEarly, "Can not redeem early");
+
+    if (notEarly) _cheemscoin.transfer(msg.sender, token.reward);
+    else availableCheems += token.reward;
+
     _lp.transfer(msg.sender, token.amount);
     _burn(id);
+    delete stakes[id];
   }
 
   /**
