4c766f1e4714d6d946650f194e3e5e9fd26ad030
kowasaur <kowasaur@gmail.com>
2021-04-30 17:07:43 +1000

    make it functional

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,27 +1,15 @@
-import {
-  Heading,
-  useBoolean,
-  useColorModeValue,
-  Flex,
-  Box,
-  Text,
-} from "@chakra-ui/react";
-import { useState } from "react";
-import { ExtLink } from "../components/ExtLink";
+import { Heading, Box, Text, Flex } from "@chakra-ui/react";
 import { Section } from "../components/Section";
 
+export default () => {
   return (
-    <>
+    <Flex h="100%">
       <Section>
         <Box maxW="container.sm" mb="5" mx="2">
           <Heading mb="1">404</Heading>
-          <Text>
-            This page does not exist or has been removed.
-          </Text>
+          <Text>This page does not exist or has been removed.</Text>
         </Box>
       </Section>
-    </>
+    </Flex>
   );
 };
-
-export default 404;
