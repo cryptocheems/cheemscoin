a4453e21ba1bfd6b4ab357d691075a60c64367bd
kowasaur <kowasaur@gmail.com>
2021-05-12 18:21:20 +1000

    update dependencies
    I just ran npm i

--- migrations/4_farm.js
+++ migrations/4_farm.js
@@ -8,14 +8,14 @@ module.exports = async (deployer, network) => {
   let lpToken;
   if (network === "development") {
     await deployer.deploy(LpTokenMock, { overwrite: false });
-    lpToken = await LpTokenMock.deployed();
+    lpToken = await LpTokenMock.deployed().address;
   } else {
-    lpToken.address = "0xce5382ff31b7a6F24797A46c307351FDE135C0Fd";
+    lpToken = "0xce5382ff31b7a6F24797A46c307351FDE135C0Fd";
   }
 
   const cheemscoin = Cheemscoin.deployed();
 
-  await deployer.deploy(LockedCheemscoinLpFarm, cheemscoin.address, lpToken.address, {
+  await deployer.deploy(LockedCheemscoinLpFarm, cheemscoin.address, lpToken, {
     overwrite: false,
   });
 
